e1d2184aced3f46201d6c0402ba447ab0d70cd6b
kkeric <ericmelo2765@gmail.com>
2025-06-11 13:20:11 -0300

    Carousel, Mudanças no HEADER e no SCROLL

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,1 +1,24 @@
 
+function carousel_back() {
+    const carousel_item_focus = document.querySelector(".carousel-item.focus");
+    carousel_item_focus.classList.remove("focus");
+
+    var previous_project = carousel_item_focus.previousElementSibling;
+
+    if (!previous_project) {
+        previous_project = carousel_item_focus.parentElement.lastElementChild;
+    }
+    previous_project.classList.add("focus");
+}
+
+function carousel_next() {
+    const carousel_item_focus = document.querySelector(".carousel-item.focus");
+    carousel_item_focus.classList.remove("focus");
+
+    var next_project = carousel_item_focus.nextElementSibling;
+
+    if (!next_project) {
+        next_project = carousel_item_focus.parentElement.firstElementChild;
+    }
+    next_project.classList.add("focus");
+}
